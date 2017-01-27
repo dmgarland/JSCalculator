5305e42ae3d7df5f8a019d2899a007e6e4c94dc8
Dan Garland <dan@dangarland.co.uk>
2017-01-27 17:30:55 +0000

    Adding a plus button

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 function equals() {
   var equation = document.getElementById("equation");
-  // equation.value = eval(equation.value);
+  equation.value = eval(equation.value);
 
-  console.log(equation.value.split(""));
+  // console.log(equation.value.split(""));
 }
 
 function number(digit) {
   console.log(digit);
+
+  var equation = document.getElementById("equation");
+
+  equation.value = equation.value + digit;
 }
 
 function plus() {
   console.log("plus");
+
+  var equation = document.getElementById("equation");
+
+  equation.value = equation.value + "+";
 }

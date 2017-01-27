fce7182c74e25b4761f8c1f6eb3263da246367f6
Dan Garland <dan@dangarland.co.uk>
2017-01-27 17:19:46 +0000

    Adding a basic calculator layout

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function equals() {
+  var equation = document.getElementById("equation");
+  // equation.value = eval(equation.value);
+
+  console.log(equation.value.split(""));
+}
+
+function number(digit) {
+  console.log(digit);
+}
+
+function plus() {
+  console.log("plus");
+}
